3d007a3be7466b9b83c10788a0d3cf9aaea6d949
bmalkes <bmalkes@gmail.com>
2025-03-27 15:21:45 +0200

    fix:change some files of dotvizion

--- dist/email/AuthEmail.js
+++ dist/email/AuthEmail.js
@@ -5,32 +5,32 @@ const nodemailer_1 = require("../config/nodemailer");
 class AuthEmail {
     static sendConfirmationEmail = async (user) => {
         const info = await nodemailer_1.transporter.sendMail({
-            from: "Differeacting <info@differeacting.com>",
+            from: "Dotvizion <info@dotvizion.com>",
             to: user.email,
-            subject: "Differeacting - Confirm account",
-            text: "Diffeacting - Confirm account",
+            subject: "Dotvizion - Confirm account",
+            text: "Dotvizion - Confirm account",
             html: `<p>Hello: ${user.name}, You have created your account in Differacting, everything is almost ready, you just have to confirm your account. </p>
         <p>Visit the following link: </p>
         <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirm account</a>
         <p>and enter the code: <b>${user.token}</b> </p>
         <p>this token expires in 10 minutes</p>
-        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1715794760/Logo_Horizontal_C_2_ducktr.png" style="width:350px" style="margin-top:30px"/>        
+        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1739727604/5000x5000-3-removebg-preview_qvlhb9.webp" style="width:350px" style="margin-top:30px"/>        
         `,
         });
         console.log(info.messageId);
     };
     static sendPasswordResetToken = async (user) => {
         const info = await nodemailer_1.transporter.sendMail({
-            from: "Differeacting <info@differeacting.com>",
+            from: "Dotvizion <info@dotvizion.com>",
             to: user.email,
-            subject: "Differeacting -Restore your password",
-            text: "Diffeacting - restore your password",
+            subject: "Dotvizion-Restore your password",
+            text: "Dotvizion - restore your password",
             html: `<p>Hello: ${user.name}, you request to change your password </p>
         <p>Visit the following link: </p>
         <a href="${process.env.FRONTEND_URL}/auth/new-password">Change Password</a>
         <p>and enter the code: <b>${user.token}</b> </p>
         <p>this token expires in 10 minutes</p>
-        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1715794760/Logo_Horizontal_C_2_ducktr.png" style="width:350px" style="margin-top:30px"/>        
+        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1739727604/5000x5000-3-removebg-preview_qvlhb9.webp" style="width:350px" style="margin-top:30px"/>        
         `,
         });
         console.log(info.messageId);
--- src/email/AuthEmail.ts
+++ src/email/AuthEmail.ts
@@ -9,16 +9,16 @@ interface IEmail {
 export class AuthEmail {
   static sendConfirmationEmail = async (user: IEmail) => {
     const info = await transporter.sendMail({
-      from: "Differeacting <info@differeacting.com>",
+      from: "Dotvizion <info@dotvizion.com>",
       to: user.email,
-      subject: "Differeacting - Confirm account",
-      text: "Diffeacting - Confirm account",
+      subject: "Dotvizion - Confirm account",
+      text: "Dotvizion - Confirm account",
       html: `<p>Hello: ${user.name}, You have created your account in Differacting, everything is almost ready, you just have to confirm your account. </p>
         <p>Visit the following link: </p>
         <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirm account</a>
         <p>and enter the code: <b>${user.token}</b> </p>
         <p>this token expires in 10 minutes</p>
-        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1715794760/Logo_Horizontal_C_2_ducktr.png" style="width:350px" style="margin-top:30px"/>        
+        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1739727604/5000x5000-3-removebg-preview_qvlhb9.webp" style="width:350px" style="margin-top:30px"/>        
         `,
     });
     console.log(info.messageId);
@@ -26,16 +26,16 @@ export class AuthEmail {
 
   static sendPasswordResetToken = async (user: IEmail) => {
     const info = await transporter.sendMail({
-      from: "Differeacting <info@differeacting.com>",
+      from: "Dotvizion <info@dotvizion.com>",
       to: user.email,
-      subject: "Differeacting -Restore your password",
-      text: "Diffeacting - restore your password",
+      subject: "Dotvizion-Restore your password",
+      text: "Dotvizion - restore your password",
       html: `<p>Hello: ${user.name}, you request to change your password </p>
         <p>Visit the following link: </p>
         <a href="${process.env.FRONTEND_URL}/auth/new-password">Change Password</a>
         <p>and enter the code: <b>${user.token}</b> </p>
         <p>this token expires in 10 minutes</p>
-        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1715794760/Logo_Horizontal_C_2_ducktr.png" style="width:350px" style="margin-top:30px"/>        
+        <img src="https://res.cloudinary.com/landingpage2/image/upload/v1739727604/5000x5000-3-removebg-preview_qvlhb9.webp" style="width:350px" style="margin-top:30px"/>        
         `,
     });
     console.log(info.messageId);
